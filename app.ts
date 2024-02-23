a142738bfa17f12234c7f83dab258392285eebb3
gideon <gideonkipkorir00@gmail.com>
2024-02-23 14:55:08 +0300

    test

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ const connectDB = async () => {
     try {
         const uri = process.env.MONGO_URI || "";
         await mongoose.connect(uri);
-        console.log("MongoDB connected successfully!");
+        console.log("MongoDB connected successfully!!!");
     } catch (error) {
         console.error("MongoDB connection error:", error);
         process.exit(1);
